168589c7c6ffe6409959128cb1d05841714d079d
takari.com <takari.com>
2021-09-08 22:55:55 +0900

    チャット

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div>
     <div><router-link to="/signin">Signin</router-link></div>
     <div><router-link to="/signup">Signup</router-link></div>
-    <div><router-link to="timeline">TimeLine</router-link></div>
-    <div><router-link to="scroll">Scroll</router-link></div>
-    <div><router-link to="chat">Chat</router-link></div>
+    <div><router-link to="/scroll">Scroll</router-link></div>
+    <div><router-link to="/chat">Chat</router-link></div>
 
     <router-view />
   </div>
